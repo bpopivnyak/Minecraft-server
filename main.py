fa1fda56b11d1c576cbb8750b2259b0c11d3d2b4
meimeuiii <141341450+bpopivnyak@users.noreply.github.com>
2024-12-22 18:29:02 +0200

    MInecraft Adobe

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import *
 
 app = Flask("The War Of World")
+app.secret_key = "28800"
 
-
-
+@app.route("/")
+def Servers():
+    return render_template("base.html")
 
 app.run()
 
