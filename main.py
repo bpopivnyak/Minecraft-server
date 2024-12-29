298c512e59c488521a2e75dd965dbf88e55720c7
meimeuiii <141341450+bpopivnyak@users.noreply.github.com>
2024-12-29 18:33:26 +0200

    Minecraft surprise

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ app = Flask("The War Of World")
 app.secret_key = "28800"
 
 @app.route("/")
-def Servers():
+def Main():
     return render_template("index.html")
+
+@app.route("/Donation")
+def DonatePage():
+    return render_template("Donation-shop.html")
+
+@app.route("/Servers")
+def ServerList():
+    return render_template("Servers.html")
+
+@app.route("/Cl@N$")
+def Active_clans():
+    return render_template("Clans.html")
+
+@app.route("/ABOUT_US")
+def Credits():
+    return render_template("about_us.html")
+
+@app.route("/Login")
+def DataPlayer():
+    return render_template("Login.html")
+
 
 app.run()
 
