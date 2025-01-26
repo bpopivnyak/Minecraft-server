bbde52cceb18980ce1487d40bec0c731beb6d3e8
Roma <141341450+bpopivnyak@users.noreply.github.com>
2025-01-26 17:24:19 +0200

    The Final

--- main.py
+++ main.py
@@ -62,5 +62,70 @@ def clanInfo8():
 @app.route("/Clan-page-9")
 def clanInfo9():
     return render_template("Cl@n-page#9.html")
+
+
+@app.route("/Minecraft_server")
+def Ms():
+    return render_template("MS_1.html")
+
+
+@app.route("/Minecraft_server2")
+def MS2():
+    return render_template("MS_2.html")
+
+
+@app.route("/Minecraft_server3")
+def MS3():
+    return render_template("MS_3.html")
+
+@app.route("/Minecraft_server4")
+def MS4():
+    return render_template("MS_4.html")
+
+
+@app.route("/Minecraft_server5")
+def MS5():
+    return render_template("MS_5.html")
+
+@app.route("/Minecraft_server6")
+def MS6():
+    return render_template("MS_6.html")
+
+@app.route("/Minecraft_server7")
+def MS7():
+    return render_template("MS_7.html")
+
+
+@app.route("/Minecraft_server8")
+def MS8():
+    return render_template("MS_8.html")
+
+@app.route("/Minecraft_server9")
+def MS9():
+    return render_template("MS_9.html")
 app.run()
+
+@app.route("/Minecraft_server10")
+def MS10():
+    return render_template("MS_10.html")
+app.run()
+
+@app.route("/Minecraft_server11")
+def MS11():
+    return render_template("MS_11.html")
+
+@app.route("/Minecraft_server12")
+def MS12():
+    return render_template("MS_12.html")
+
+@app.route("/Minecraft_server13")
+def MS13():
+    return render_template("MS_13.html")
+
+@app.route("/Minecraft_server14")
+def MS14():
+    return render_template("MS_14.html")
+app.run()
+
+
 
